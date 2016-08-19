22fb61717407d0cb44326f192d2515247a5d5734
Armin Biere <armin.biere@gmail.com>
2016-08-19 14:27:02 -0400

    added license to .cc file

--- cadical.cc
+++ cadical.cc
@@ -1,5 +1,33 @@
+/*--------------------------------------------------------------------------
+Radically Simplified Conflict Driven Clause Learning Solver (CDCL)
 
-/*------------------------------------------------------------------------*/
+The goal of CaDiCal is to have a minimalistic CDCL solver,
+which is easy to understand and change, while at the same
+time not too much slower than state of the art CDCL solvers
+if pre-processing is disabled.
+
+MIT License
+
+Copyright (c) 2016 Armin Biere, JKU.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+--------------------------------------------------------------------------*/
 
 // Standard C includes
 
