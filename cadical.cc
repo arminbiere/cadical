3d19c916a6e4979802c5495beb036c94e85e347d
Armin Biere <armin.biere@gmail.com>
2016-10-02 18:44:56 -0700

    back

--- cadical.cc
+++ cadical.cc
@@ -112,11 +112,11 @@ struct Clause {
 
   long resolved;     // time stamp when resolved last time
                     
-  bool redundant:1;    // aka 'learned' so not 'irredundant' (original)
-  bool garbage:1;      // can be garbage collected unless it is a 'reason'
-  bool reason:1;       // reason / antecedent clause can not be collected
+  bool redundant;    // aka 'learned' so not 'irredundant' (original)
+  bool garbage;      // can be garbage collected unless it is a 'reason'
+  bool reason;       // reason / antecedent clause can not be collected
 
-  unsigned glue :28;          // glue = glucose level = LBD
+  int glue;          // glue = glucose level = LBD
   int size;          // actual size of 'literals' (at least 2)
 
   int literals[2];   // actually of variadic 'size' in general
