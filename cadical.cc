3fad4e73af35bf100a86c887ad678142fe246fd8
Armin Biere <armin.biere@gmail.com>
2016-10-02 18:39:39 -0700

    fixed comments

--- cadical.cc
+++ cadical.cc
@@ -110,17 +110,16 @@ static struct {
 
 struct Clause {
 
-  long resolved;
+  long resolved;     // time stamp when resolved last time
+                    
+  bool redundant;    // aka 'learned' so not 'irredundant' (original)
+  bool garbage;      // can be garbage collected unless it is a 'reason'
+  bool reason;       // reason / antecedent clause can not be collected
 
-  bool redundant; // aka 'learned' so not 'irredundant' (original)
-  bool garbage;   // can be garbage collected unless it is a 'reason'
-  bool reason;    // reason / antecedent clause can not be collected
+  int glue;          // glue = glucose level = LBD
+  int size;          // actual size of 'literals' (at least 2)
 
-  int glue;             // glue = glucose level = LBD
-  int size;             // actual size of 'literals' (at least 2)
-
-  int literals[2];      // actually of variadic 'size' in general
-                        // for binary embedded reason clauses 'size == 2'
+  int literals[2];   // actually of variadic 'size' in general
 };
 
 /*------------------------------------------------------------------------*/
