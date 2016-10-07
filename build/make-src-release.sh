3920ed723b14bd0f8923f7785a09135c91cd0018
Armin Biere <armin.biere@gmail.com>
2016-10-07 10:49:16 -0700

    fixed make-src-release.sh

--- build/make-src-release.sh
+++ build/make-src-release.sh
@@ -1,22 +1,18 @@
 #!/bin/sh
-version="`cat VERSION`"
+version="`cat ../VERSION`"
 gitid="`./get-git-id.sh|sed -e 's,^\(.......\).*,\1,'`"
 name=cadical-$version-$gitid
 dir=/tmp/$name
 tar=/tmp/$name.tar.bz2
 rm -rf $dir
 mkdir $dir
-cp -p \
-cadical.cc \
-configure.sh \
-LICENSE \
-makefile.in \
-make-config-header.sh \
-README.md \
-VERSION \
-$dir
-sed -i -e 's,`./get-git-id.sh`,'"`./get-git-id.sh`", $dir/make-config-header.sh
-sed -i -e '/rm -f test/d' $dir/makefile.in
+cp -p ../LICENSE ../README.md ../VERSION $dir
+mkdir $dir/src
+cp -p ../src/*.[ch]pp $dir/src
+mkdir $dir/build
+cp -p makefile.in *.sh $dir/build
+sed -i -e 's,`./get-git-id.sh`,'"`./get-git-id.sh`", $dir/build/make-config-header.sh
+sed -i -e '/rm -f test/d' $dir/build/makefile.in
 cd /tmp
 tar jcf $tar $name
 ls -l $tar
