1975a533193d6bac960506497aa3c2d26acb13ca
Armin Biere <biere@jku.at>
2016-10-14 06:22:21 +0200

    almost back to previous propagate

--- src/propagate.cpp
+++ src/propagate.cpp
@@ -26,40 +26,37 @@ void Internal::assign (int lit, Clause * reason) {
 bool Internal::propagate () {
   assert (!unsat);
   START (propagate);
-  long before = propagated;
   while (!conflict && propagated < trail.size ()) {
-    const int lit = trail[propagated++];
-    LOG ("propagating %d", lit);
-    Watches & ws = watches (-lit);
+    stats.propagations++;
+    const int lit = -trail[propagated++];
+    LOG ("propagating %d", -lit);
+    Watches & ws = watches (lit);
     const_watch_iterator i = ws.begin ();
     watch_iterator j = ws.begin ();
     while (i != ws.end ()) {
       const Watch w = *j++ = *i++;
-      const int blit = w.blit, b = val (blit);
+      const int b = val (w.blit);
       if (b > 0) continue;
-      Clause * c = w.clause;
-      const int size = w.size;
-      if (size == 2) {
-        if (b < 0) conflict = c;
-        else if (!b) assign (blit, c);
+      if (w.size == 2) {
+        if (b < 0) conflict = w.clause;
+        else if (!b) assign (w.blit, w.clause);
       } else {
-        int * lits = c->literals;
-        if (lits[1] != -lit) swap (lits[0], lits[1]);
-        assert (lits[1] == -lit);
+        int * lits = w.clause->literals;
+        if (lits[0] == lit) swap (lits[0], lits[1]);
         const int u = val (lits[0]);
         if (u > 0) j[-1].blit = lits[0];
         else {
           int k, v = -1;
-          for (k = 2; k < size && (v = val (lits[k])) < 0; k++)
+          for (k = 2; k < w.size && (v = val (lits[k])) < 0; k++)
             ;
           if (v > 0) j[-1].blit = lits[k];
           else if (!v) {
-            LOG (c, "unwatch %d in", -lit);
-            swap (lits[1], lits[k]);
-            watch_literal (lits[1], -lit, c, size);
+            LOG (c, "unwatch %d in", lit);
+	    swap (lits[1], lits[k]);
+            watch_literal (lits[1], lit, w.clause, w.size);
             j--;
-          } else if (!u) assign (lits[0], c);
-          else { conflict = c; break; }
+          } else if (!u) assign (lits[0], w.clause);
+          else { conflict = w.clause; break; }
         }
       }
     }
@@ -67,7 +64,6 @@ bool Internal::propagate () {
     ws.resize (j - ws.begin ());
   }
   if (conflict) { stats.conflicts++; LOG (conflict, "conflict"); }
-  stats.propagations += trail.size () - before;
   STOP (propagate);
   return !conflict;
 }
