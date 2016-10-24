b0f99b3bc375b3e4bed41c4229007cae4f38bc78
Armin Biere <biere@jku.at>
2016-10-24 07:59:51 +0200

    sublast optimization

--- src/analyze.cpp
+++ src/analyze.cpp
@@ -166,17 +166,18 @@ void Internal::clear_levels () {
 
 inline bool Internal::eagerly_subsume_last_learned (Clause * c) {
   const_literal_iterator end = c->end ();
-  size_t found = 0;
+  size_t found = 0, remain = c->size - clause.size ();
   for (const_literal_iterator i = c->begin (); i != end; i++) {
     int tmp = marked (*i);
     if (tmp < 0) break;
-    if (tmp > 0) found++;
+    else if (tmp > 0) found++;
+    else if (!remain--) break;
   }
   assert (found <= clause.size ());
   if (found < clause.size ()) return false;
   LOG (c, "learned clauses eagerly subsumes");
-  c->garbage = true;
   assert (c->redundant);
+  c->garbage = true;
   stats.sublast++;
   return true;
 }
@@ -190,12 +191,12 @@ void Internal::eagerly_subsume_last_learned () {
   for (int j = 0; j < opts.sublast; j++) {
     if (i == clauses.begin ()) break;
     Clause * c = *--i;
-    if (!c->redundant) break;
     if (c->garbage) continue;
+    if (!c->redundant) continue;
     if ((size_t) c->size <= clause.size ()) continue;
-    tried++;
     LOG (c, "trying to eagerly subsume");
     if (eagerly_subsume_last_learned (c)) subsumed++;
+    tried++;
   }
   for (k = clause.begin (); k != clause.end (); k++) unmark (*k);
   LOG ("subsumed eagerly %d clauses out of %d tried", subsumed, tried);
