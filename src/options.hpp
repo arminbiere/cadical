b072f72b570a148f48fb429acdbe8d64930dfbf6
Armin Biere <biere@jku.at>
2016-11-14 21:20:24 +0100

    much less aggressive elim and subsume limits

--- src/options.hpp
+++ src/options.hpp
@@ -27,11 +27,11 @@ OPTION(check,           bool,DEBUG, 0,  1, "save & check original CNF") \
 OPTION(clim,             int,   -1, 0,1e9, "conflict limit (-1=none)") \
 OPTION(dlim,             int,   -1, 0,1e9, "decision limit (-1=none)") \
 OPTION(elim,            bool,    1, 0,  1, "bounded variable elimination") \
-OPTION(elimclslim,       int,  1e9, 0,1e9, "ignore clauses of this size") \
+OPTION(elimclslim,       int, 1000, 0,1e9, "ignore clauses of this size") \
 OPTION(elimignore,    double,  0.0, 0,  1, "ignored fraction of variables") \
 OPTION(eliminit,         int,  1e3, 0,1e9, "initial conflict limit") \
 OPTION(elimint,          int,  1e4, 1,1e9, "initial conflict interval") \
-OPTION(elimocclim,       int,  100, 0,1e9, "one sided occurrence limit") \
+OPTION(elimocclim,       int,   20, 0,1e9, "one sided occurrence limit") \
 OPTION(elimroundsinit,   int,    5, 1,1e9, "initial number of rounds") \
 OPTION(elimrounds,       int,    2, 1,1e9, "usual number of rounds") \
 OPTION(emabumplast,   double, 1e-5, 0,  1, "alpha bump last percentage") \
@@ -65,10 +65,10 @@ OPTION(shrinksize,       int,   20, 0,1e9, "size limit for shrinking") \
 OPTION(strengthen,      bool,    1, 0,  1, "strengthen during subsume") \
 OPTION(sublast,          int,    5, 0,1e4, "eagerly subsume last") \
 OPTION(subsume,         bool,    1, 0,  1, "enable clause subsumption") \
-OPTION(subsumeclslim,    int,  1e9, 0,1e9, "clause length limit") \
+OPTION(subsumeclslim,    int, 1000, 0,1e9, "clause length limit") \
 OPTION(subsumeinc,       int,  1e4, 1,1e9, "interval in conflicts") \
 OPTION(subsumeinit,      int,  1e4, 0,1e9, "initial subsume limit") \
-OPTION(subsumeocclim,    int,  100, 0,1e9, "watch list length limit") \
+OPTION(subsumeocclim,    int,   20, 0,1e9, "watch list length limit") \
 OPTION(trailbump,       bool,    1, 0,  1, "use trail + bumped") \
 OPTION(trailbumplast, double,   40, 0,100, "trail bump last level limit") \
 OPTION(trailbumprops, double,  200, 0,1e9, "trail bump propagation limit") \
