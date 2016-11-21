5f9e146c4f475c55ac9343519597398d5ff9500c
Armin Biere <armin.biere@gmail.com>
2016-11-21 19:28:27 +0100

    back to vector for occurrences and watches

--- src/occs.hpp
+++ src/occs.hpp
@@ -2,22 +2,16 @@
 #define _occs_h_INCLUDED
 
 #include <vector>
-#include "cector.hpp"
 
 namespace CaDiCaL {
 
 class Clause;
 using namespace std;
 
-#if 0
 typedef vector<Clause*> Occs;
+
 inline void shrink_occs (Occs & os) { shrink_vector (os); }
 inline void erase_occs (Occs & os) { erase_vector (os); }
-#else
-typedef cector<Clause*> Occs;
-inline void shrink_occs (Occs & os) { os.shrink (); }
-inline void erase_occs (Occs & os) { os.resize (0); os.shrink (); }
-#endif
 
 typedef Occs::iterator occs_iterator;
 typedef Occs::const_iterator const_occs_iterator;
--- src/watch.hpp
+++ src/watch.hpp
@@ -2,9 +2,9 @@
 #define _watch_hpp_INCLUDED
 
 #include "clause.hpp"
-#include "cector.hpp"
 
 #include <cassert>
+#include <vector>
 
 namespace CaDiCaL {
 
@@ -20,17 +20,9 @@ struct Watch {
   Watch () { }
 };
 
-};
-
-namespace CaDiCaL {
-
-#if 0
 typedef vector<Watch> Watches;          // of one literal
+
 inline void shrink_watches (Watches & ws) { shrink_vector (ws); }
-#else
-typedef cector<Watch> Watches;
-inline void shrink_watches (Watches & ws) { ws.shrink (); }
-#endif
 
 typedef Watches::iterator watch_iterator;
 typedef Watches::const_iterator const_watch_iterator;
