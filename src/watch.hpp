a09590d14987828b538c169d9fe7612ec5154919
Armin Biere <armin.biere@gmail.com>
2016-11-21 19:25:30 +0100

    removed cector

--- src/watch.hpp
+++ src/watch.hpp
@@ -2,7 +2,6 @@
 #define _watch_hpp_INCLUDED
 
 #include "clause.hpp"
-#include "cector.hpp"
 
 #include <cassert>
 
@@ -20,17 +19,9 @@ struct Watch {
   Watch () { }
 };
 
-};
-
-namespace CaDiCaL {
-
-#if 0
 typedef vector<Watch> Watches;          // of one literal
+
 inline void shrink_watches (Watches & ws) { shrink_vector (ws); }
-#else
-typedef cector<Watch> Watches;
-inline void shrink_watches (Watches & ws) { ws.shrink (); }
-#endif
 
 typedef Watches::iterator watch_iterator;
 typedef Watches::const_iterator const_watch_iterator;
