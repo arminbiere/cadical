c4eac7d6f864908d636cc658cf725470581ccfe2
Armin Biere <biere@jku.at>
2016-11-26 10:00:20 +0100

    worked on heap

--- src/heap.hpp
+++ src/heap.hpp
@@ -4,55 +4,57 @@
 #include <cassert>
 #include <vector>
 
-template<class T, class I, class M, class C> class Heap {
+using namespace std;
 
-  using namespace std;
+template<class C> class int_heap {
 
-  vector<T> array;	// actual binary heap
-  vector<I> pos;	// positions of T objects in heap
+  vector<int> array;	// actual binary heap
+  vector<int> pos;	// positions of int objects in heap
 
-  M mapper;		// maps T objects to I indices
-  C compare;		// less-than for T objects
+  C less;		// less-than for int objects
 
-  I & index (T t) {
-    I & res = mapper () (t);
-    assert (res < 0 || res < (I) array.size ());
+  int & index (int t) {
+    assert (t > 0);
+    while (t >= (int) pos.size ()) pos.push_back (-1);
+    int & res = pos[t];
+    assert (res < 0 || res < (int) array.size ());
     return res;
   }
 
 public:
 
-  bool contains (T t) { return index (t) >= 0; }
+  bool contains (int t) { 
+    if (t >= (int) pos.size ()) return false;
+    return pos[t] >= 0;
+  }
 
 private:
 
-  bool has_parent (T t)      { return index (p) > 0; }
-  bool has_left_child (T t)  { return 2*index (t) + 1 < (I) array.size (); }
-  bool has_right_child (T t) { return 2*index (t) + 2 < (I) array.size (); }
+  bool has_parent (int t)      { return index (t) > 0; }
+  bool has_left_child (int t)  { return 2*index (t) + 1 < (int) array.size (); }
+  bool has_right_child (int t) { return 2*index (t) + 2 < (int) array.size (); }
 
-  T parent (T t) { assert (has_parent (t));      return array[(index (t) - 1)/2]; }
-  T left (T t)   { assert (has_left_child (t));  return array[2*index (t) + 1]; }
-  T right (T t)  { assert (!has_right_child (t)) return array[2*index (t) + 1]; }
+  int parent (int t) { assert (has_parent (t));      return array[(index (t) - 1)/2]; }
+  int left (int t)   { assert (has_left_child (t));  return array[2*index (t) + 1]; }
+  int right (int t)  { assert (has_right_child (t)); return array[2*index (t) + 1]; }
 
-  bool less (T a, T b) { return compare () (a, b); }
-
-  void bubble (T a, T b) {
-    I & p = index (a), & q = index (b);
+  void bubble (int a, int b) {
+    int & p = index (a), & q = index (b);
     swap (array[p], array[q]);
     swap (p, q);
   }
 
-  void up (T t) {
-    T p;
+  void up (int t) {
+    int p;
     while (has_parent (t) && less ((p = parent (t)), t))
       bubble (p, t);
   }
 
-  void down (T t) {
+  void down (int t) {
     while (has_left_child (t)) {
-      T c = left (t);
+      int c = left (t);
       if (has_right_child (t)) {
-	T r = right (t);
+	int r = right (t);
 	if (less (c, r)) c = r;
       }
       if (!less (t, c)) break;
@@ -62,15 +64,26 @@ private:
 
 public:
 
-  typedef vector>iterator;
+  int_heap (const C & c) : less (c) { }
 
-  Heap (M m, C c) : mapper (m), compare (c) { }
-
-  bool contains (T t) { index (t) >= 0; }
-
-  void push_back (T t) {
-    assert (contains (t));
+  void push_back (int t) {
+    assert (0 < t);
+    assert (!contains (t));
+    int i = (int) array.size ();
+    array.push_back (t);
+    index (t) = i;
+    up (t);
+    down (t);
   }
+
+  typedef typename vector<int>::iterator iterator;
+  typedef typename vector<int>::const_iterator const_iterator;
+
+  iterator begin () { return array.begin (); }
+  iterator end () { return array.end (); }
+
+  const_iterator begin () const { return array.begin (); }
+  const_iterator end () const { return array.end (); }
 };
 
 #endif
