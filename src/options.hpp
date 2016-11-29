6f26d417877f188b205a263792ed5d902ebdb616
Armin Biere <biere@jku.at>
2016-11-29 20:10:44 +0100

    disabled

--- src/options.hpp
+++ src/options.hpp
@@ -22,10 +22,11 @@
 /*     NAME             TYPE, VAL, LO, HI, USAGE */ \
 \
 OPTION(arena,            int,    3, 0,  3, "1=clause,2=var,3=queue") \
-OPTION(block,           bool,    0, 0,  1, "blocked clause elimination") \
+OPTION(block,           bool,    1, 0,  1, "blocked clause elimination") \
+OPTION(blockeepsize,     int,    3, 0,1e9, "max size kept blocked clauses") \
+OPTION(blockminclsize,   int,    5, 0,1e9, "minimum clause size for BVE") \
 OPTION(blockreleff,   double, 0.10, 0,  1, "relative block efficiency") \
 OPTION(blockmineff,   double,  1e6, 0,  1, "minimum block efficiency") \
-OPTION(blockeepsize,     int,    3, 0,1e9, "max size kept blocked clauses") \
 OPTION(blockwait,        int,    0, 0,1e9, "wait for blocking phase") \
 OPTION(binary,          bool,    1, 0,  1, "use binary proof format") \
 OPTION(check,           bool,DEBUG, 0,  1, "save & check original CNF") \
