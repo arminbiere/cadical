8cfba504b57c640ad66c3a0b4cc83e818d70c32d
Armin Biere <biere@jku.at>
2016-11-30 07:29:23 +0100

    enabled blocked clauses again

--- src/options.hpp
+++ src/options.hpp
@@ -22,9 +22,9 @@
 /*     NAME             TYPE, VAL, LO, HI, USAGE */ \
 \
 OPTION(arena,            int,    3, 0,  3, "1=clause,2=var,3=queue") \
-OPTION(block,           bool,    0, 0,  1, "blocked clause elimination") \
+OPTION(block,           bool,    1, 0,  1, "blocked clause elimination") \
 OPTION(blockeepsize,     int,    3, 0,1e9, "max size kept blocked clauses") \
-OPTION(blockminclsize,   int,    5, 0,1e9, "minimum clause size for BVE") \
+OPTION(blockminclsize,   int,    2, 0,1e9, "minimum clause size for BVE") \
 OPTION(blockreleff,   double, 0.10, 0,  1, "relative block efficiency") \
 OPTION(blockmineff,   double,  1e6, 0,  1, "minimum block efficiency") \
 OPTION(blockwait,        int,    0, 0,1e9, "wait for blocking phase") \
