77efcc0cceeaa8c6cb749852537a88fe3a37eaa8
Armin Biere <biere@jku.at>
2016-12-02 18:32:51 +0100

    level

--- src/level.hpp
+++ src/level.hpp
@@ -5,15 +5,17 @@
 
 namespace CaDiCaL {
 
-// For each new decision we increase the decision level
-// and push a 'Level' on the 'control' stack.  This is used in
+// For each new decision we increase the decision level and push a 'Level'
+// on the 'control' stack.  The information gather here is used in
 // 'reuse_trail' and for early aborts in clause minimization.
 
 struct Level {
   int decision;         // decision literal of level
   int seen;             // how many variables seen during 'analyze'
   int trail;            // smallest trail position seen
+
   void reset () { seen = 0, trail = INT_MAX; }
+
   Level (int d) : decision (d) { reset (); }
   Level () { }
 };
