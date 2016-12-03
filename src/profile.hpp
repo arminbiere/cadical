fb052937bbc29613eefc707a922ea3691b637d09
Armin Biere <biere@jku.at>
2016-12-03 15:17:35 +0100

    merged timer into profile

--- src/profile.hpp
+++ src/profile.hpp
@@ -7,21 +7,6 @@ class Internal;
 
 /*------------------------------------------------------------------------*/
 
-// There is a timer stack for profiling functions.
-
-struct Timer {
-
-  double started;       // starting time (in seconds) for this phase
-  Profile * profile;    // update this profile if phase stops
-
-  Timer (double s, Profile * p) : started (s), profile (p) { }
-  Timer () { }
-
-  void update (double now) { profile->value += now - started; started = now; }
-};
-
-/*------------------------------------------------------------------------*/
-
 // The solver contains some built in profiling (even for optimized code).
 // The idea is that even without using external tools it is possible to get
 // an overview of where time is spent.  This is enabled with the option
@@ -100,6 +85,23 @@ struct Profile {
 
   Profile (const char * n, int l) : value (0), name (n), level (l) { }
 };
+
+/*------------------------------------------------------------------------*/
+
+// There is a timer stack for profiling functions.
+
+struct Timer {
+
+  double started;       // starting time (in seconds) for this phase
+  Profile * profile;    // update this profile if phase stops
+
+  Timer (double s, Profile * p) : started (s), profile (p) { }
+  Timer () { }
+
+  void update (double now) { profile->value += now - started; started = now; }
+};
+
+/*------------------------------------------------------------------------*/
 
 struct Profiles {
   Internal * internal;
