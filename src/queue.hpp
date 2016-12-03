06f9c1217672d067713158ff9922fcb3abd3392e
Armin Biere <biere@jku.at>
2016-12-03 14:26:51 +0100

    queue

--- src/queue.hpp
+++ src/queue.hpp
@@ -3,28 +3,36 @@
 
 namespace CaDiCaL {
 
-// VMTF decision queue ordered by 'bumped'.
+// Variable move to front (VMTF) decision queue ordered by 'bumped'.  See
+// our SAT'15 paper for an explanation on how this works.
 
 struct Queue {
 
+  // We use integers instead of variable pointers.  This is more compact and
+  // also avoids issues due to moving the variable table during 'resize'.
+
   int first, last;    // anchors (head/tail) for doubly linked list
   int unassigned;     // all variables after this one are assigned
-  long bumped;        // bumped stamp of 'bassigned'
+  long bumped;        // see 'Internal.update_queue_unassigned'
 
   Queue () : first (0), last (0), unassigned (0), bumped (0) { }
 
-  void dequeue (Link * ltab, Link * l) {
+  // We explicitly provide the mapping of integer indices to links to the
+  // following two (inlined) functions.  This avoids including
+  // 'internal.hpp' and breaks a cyclic dependency, so we can keep their
+  // code here in this header file.  Otherwise they are just ordinary doubly
+  // linked list 'dequeue' and 'enqueue' operations.
+
+  inline void dequeue (Link * ltab, Link * l) {
     if (l->prev) ltab[l->prev].next = l->next; else first = l->next;
     if (l->next) ltab[l->next].prev = l->prev; else last = l->prev;
   }
 
-  void enqueue (Link * ltab, Link * l) {
+  inline void enqueue (Link * ltab, Link * l) {
     if ((l->prev = last)) ltab[last].next = l - ltab; else first = l - ltab;
     last = l - ltab;
     l->next = 0;
   }
-
-  void init (Internal *, int new_max_var);
 };
 
 };
