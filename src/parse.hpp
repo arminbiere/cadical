665f689094898104be6210a073939c2dabf55a1b
Armin Biere <biere@jku.at>
2016-12-03 13:09:43 +0100

    parse

--- src/parse.hpp
+++ src/parse.hpp
@@ -3,22 +3,40 @@
 
 namespace CaDiCaL {
 
+// Factors out common functions for parsing of DIMACS and solution files.
+
 class File;
 class Internal;
 
 class Parser {
+
   Internal * internal;
   File * file;
+
   void perr (const char * fmt, ...);
   int parse_char ();
+
   const char * parse_string (const char * str, char prev);
   const char * parse_positive_int (int & ch, int & res, const char * name);
   const char * parse_lit (int & ch, int & lit, const int vars);
   const char * parse_dimacs_non_profiled ();
   const char * parse_solution_non_profiled ();
+
 public:
+
   Parser (Internal * s, File * f) : internal (s), file (f) { }
+
+  // Parse a DIMACS file.  Return zero if successful. Otherwise parse error.
+  // The clauses are added.
+  //
   const char * parse_dimacs ();
+
+  // Parse a solution file as used in the SAT competition, e.g., with
+  // comment lines 'c ...', a status line 's ...' and value lines 'v ...'.
+  // Returns zero if successful. Otherwise a string is returned describing
+  // the parse error.  The parsed solution is saved in 'solution' and can be
+  // accessed with 'sol (int lit)'.  We use it for checking learned clauses.
+  //
   const char * parse_solution ();
 };
 
