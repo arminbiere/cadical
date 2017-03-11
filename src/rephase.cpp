82b016497dbe00197e31ee9766b041d739a37f45
Armin Biere <biere@jku.at>
2017-03-11 11:29:25 +0100

    added

---
+++ src/rephase.cpp
@@ -0,0 +1,22 @@
+#include "internal.hpp"
+
+namespace CaDiCaL {
+
+bool Internal::rephasing () {
+  if (!opts.rephase) return false;
+  return stats.conflicts > lim.rephase;
+}
+
+void Internal::rephase () {
+  stats.rephased++;
+  LOG ("rephase %ld", stats.rephase);
+  signed char val = opts.phase ? 1 : -1;
+  if (stats.rephased & 1) val = -val;
+  for (int idx = 1; idx <= max_var; idx++) phases[idx] = val;
+  backtrack ();
+  inc.rephase *= 2;
+  lim.rephase = stats.conflicts + inc.rephase;
+  LOG ("next rephase after %ld conflicts", lim.rephase);
+}
+
+};
