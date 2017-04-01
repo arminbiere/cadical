b9f4bf7074e39fa55c2e8603f5e50dbaee6689b1
Armin Biere <biere@jku.at>
2017-04-01 16:40:43 +0200

    closer to 048

--- src/clause.cpp
+++ src/clause.cpp
@@ -119,7 +119,7 @@ Clause * Internal::new_clause (bool red, int glue) {
   ptr -= offset;
   c = (Clause*) ptr;
 
-  if (have_analyzed) c->_analyzed = 0;
+  if (have_analyzed) c->_analyzed = ++stats.analyzed;
   if (have_pos) c->_pos = 2, c->dummy = 0;
   c->have_analyzed = have_analyzed;
   c->have_pos = have_pos;
