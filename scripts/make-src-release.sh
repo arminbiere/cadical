0b325d196bb77f1e4cbba0e2609cc573aea50421
Armin Biere <biere@jku.at>
2017-06-07 19:17:47 +0200

    added

--- scripts/make-src-release.sh
+++ scripts/make-src-release.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
-cd `dirname $0`/../build
-version="`cat ../VERSION`"
-gitid="`./get-git-id.sh|sed -e 's,^\(.......\).*,\1,'`"
-name=cadical-$version-$gitid
+cd `dirname $0`/..
+root=`pwd`
+version="`cat VERSION`"
+gitid="`./scripts/get-git-id.sh|sed -e 's,^\(.......\).*,\1,'`"
+name=cadical-${version}-${gitid}
 dir=/tmp/$name
 tar=/tmp/$name.tar.xz
 rm -rf $dir
-mkdir $dir
-cp -p ../LICENSE ../README.md ../VERSION $dir
-mkdir $dir/src
-cp -p ../src/*.[ch]pp $dir/src
-mkdir $dir/build
-cp -p makefile.in configure.sh make-config-header.sh $dir/build
-sed -i -e 's,`./get-git-id.sh`,'"`./get-git-id.sh`", $dir/build/make-config-header.sh
+mkdir $dir || exit 1
+cd $dir || exit 1
+git clone $root $dir || exit 1
+rm -rf /tmp/$name/.git
+sed -i -e 's,`./scripts/get-git-id.sh`,'"`./scripts/get-git-id.sh`", $dir/build/make-config-header.sh
 sed -i -e '/rm -f test/d' $dir/build/makefile.in
 cd /tmp
 tar cJf $tar $name
---
+++ scripts/prepare-sc2017-submission.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+cwd=`pwd`
+tmp=/tmp/prepare-sc2015-submission.log
+rm -f $tmp
+VERSION=`cat VERSION`
+base=cadical-${VERSION}-sc2017-main
+dir=/tmp/$base
+rm -rf $dir
+mkdir $dir
+mkdir $dir/bin
+mkdir $dir/build
+mkdir $dir/archives
+./scripts/make-src-release.sh | tee $tmp
+tar=`awk '{print $NF}' $tmp`
+cp -a $tar $dir/archives
+cat <<EOF >$dir/build/build.sh
+#!/bin/sh
+tar xf ../archives/cadical*
+mv cadical* cadical
+cd cadical
+./configure
+make
+install -s build/cadical ../../bin/
+EOF
+chmod 755 $dir/build/build.sh
+cat <<EOF >$dir/starexec_build
+#!/bin/sh
+cd build
+exec ./build.sh
+EOF
+chmod 755 $dir/starexec_build
+cat <<EOF >$dir/bin/starexec_run_default
+#!/bin/sh
+exec ./cadical \$1 \$2/proof.out
+EOF
+chmod 755 $dir/bin/starexec_run_default
+archive=/tmp/$base.zip
+rm -f $archive
+cd $dir
+zip -r $archive .
+cd /tmp/
+ls -l $archive
+rm -f $tmp
+rm -rf $dir/
+##########################################################################
+cd $cwd
+VERSION=`cat VERSION`
+base=cadical-${VERSION}-sc2017-notrace
+dir=/tmp/$base
+rm -rf $dir
+mkdir $dir
+mkdir $dir/bin
+mkdir $dir/build
+mkdir $dir/archives
+cp -a $tar $dir/archives
+cat <<EOF >$dir/build/build.sh
+#!/bin/sh
+tar xf ../archives/cadical*
+mv cadical* cadical
+cd cadical
+./configure
+make
+install -s build/cadical ../../bin/
+EOF
+chmod 755 $dir/build/build.sh
+cat <<EOF >$dir/starexec_build
+#!/bin/sh
+cd build
+exec ./build.sh
+EOF
+chmod 755 $dir/starexec_build
+cat <<EOF >$dir/bin/starexec_run_default
+#!/bin/sh
+exec ./cadical \$1
+EOF
+chmod 755 $dir/bin/starexec_run_default
+archive=/tmp/$base.zip
+rm -f $archive
+cd $dir
+zip -r $archive .
+cd /tmp/
+ls -l $archive
+rm -f $tmp
+rm -rf $dir/
