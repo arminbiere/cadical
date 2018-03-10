39b2d3388d95b010d44923e1b15377523454c828
Armin Biere <biere@jku.at>
2018-03-10 14:59:59 +0100

    replaced "-n" option for "echo" by printf

--- test/run-regression.sh
+++ test/run-regression.sh
@@ -65,7 +65,7 @@ run () {
     proofopts=" ../test/cnfs/$1.proof"
   fi
   opts="../test/cnfs/$1.cnf$solopts$proofopts"
-  echo -n "$binary $opts # $2 ..."
+  printf "$binary $opts # $2 ..."
   "$binary" $opts 1>cnfs/$1.log 2>cnfs/$1.err
   res=$?
   if [ $res = $2 ]
