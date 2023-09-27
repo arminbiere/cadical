90abcf0e90687ec3b3445066c97c904762656e9d
Florian <florian.pollitt@mars.uni-freiburg.de>
2023-09-27 10:21:44 +0200

    fixed 0 being in constraint

--- src/external.cpp
+++ src/external.cpp
@@ -254,7 +254,8 @@ void External::constrain (int elit) {
   }
   assert (elit != INT_MIN);
   reset_extended ();
-  constraint.push_back (elit);
+  if (elit)
+    constraint.push_back (elit);
   const int ilit = internalize (elit);
   assert (!elit == !ilit);
   if (elit)
