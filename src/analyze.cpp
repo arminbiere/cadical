0f1055dd7b9a1cdd52fb1ba6afa32aa355ed3c66
Armin Biere <biere@informatik.uni-freiburg.de>
2024-02-15 21:24:28 +0100

    more reasonbumping in stable

--- src/analyze.cpp
+++ src/analyze.cpp
@@ -359,8 +359,7 @@ inline void Internal::bump_also_all_reason_literals () {
   assert (opts.bumpreasondepth > 0);
   LOG ("bumping reasons up to depth %d", opts.bumpreasondepth);
   for (const auto &lit : clause)
-    bump_also_reason_literals (-lit,
-                               opts.bumpreasondepth); // + stable); TODO
+    bump_also_reason_literals (-lit, opts.bumpreasondepth+ stable);
 }
 
 /*------------------------------------------------------------------------*/
