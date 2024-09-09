1b95523deeb635ebbd266665879b56b821dd7f3f
Florian Pollitt <pollittf@informatik.uni-freiburg.de>
2024-09-09 09:43:52 +0200

    revert to default options

--- src/options.hpp
+++ src/options.hpp
@@ -133,7 +133,7 @@ OPTION( minimizedepth,   1e3,  0,1e3,0,0,1, "minimization depth") \
 OPTION( otfs,              1,  0,  1,0,0,1, "on-the-fly self subsumption") \
 OPTION( phase,             1,  0,  1,0,0,1, "initial phase") \
 OPTION( probe,             1,  0,  1,0,1,1, "failed literal probing" ) \
-OPTION( probehbr,          0,  0,  1,0,0,1, "learn hyper binary clauses") \
+OPTION( probehbr,          1,  0,  1,0,0,1, "learn hyper binary clauses") \
 OPTION( probeint,        5e3,  1,2e9,0,0,1, "probing interval" ) \
 OPTION( probemaxeff,     1e8,  0,2e9,1,0,1, "maximum probing efficiency") \
 OPTION( probemineff,     1e6,  0,2e9,1,0,1, "minimum probing efficiency") \
