ac946a3b5d646af6ed9f23b410d1cfd2aa670fff
Tobias Faller <fallert@tf.uni-freiburg.de>
2024-10-28 16:54:55 +0100

    Fixed condition where profiling is still active in case of a failed allocation

--- src/profile.hpp
+++ src/profile.hpp
@@ -133,6 +133,10 @@ struct Profiles {
                                       internal->time ());) \
   } while (0)
 
+#define PROFILE_ACTIVE(P) \
+  ((internal->profiles.P.level <= internal->opts.profile) \
+    && (internal->profiles.P.active))
+
 /*------------------------------------------------------------------------*/
 
 #define START_SIMPLIFIER(S, M) \
