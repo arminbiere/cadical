2d5bfb5d32cdc5289676c3d0056ca429dd400ab5
Mathias Fleury <Mathias.Fleury@ens-rennes.fr>
2025-02-17 20:21:33 +0100

    fix regression test

--- test/api/learn.cpp
+++ test/api/learn.cpp
@@ -50,7 +50,10 @@ static void formula (CaDiCaL::Solver &solver) {
 int main () {
   CaDiCaL::Solver ping, pong;
   ping.set ("log", 1), pong.set ("log", 1);
+  ping.set ("congruence", 0), pong.set ("congruencexor", 0);
+  ping.set ("sweep", 0), pong.set ("sweep", 0);
   ping.set ("otfs", 0), pong.set ("otfs", 0);
+  ping.set ("factor", 0), pong.set ("factor", 0);
   Wrapper wing (&ping), wong (&pong);
   formula (ping), formula (pong);
   int a = ping.solve ();
