caebb4a7f37a26ff859c66fa7479308bc5bf50fa
Florian <pollittf@informatik.uni-freiburg.de>
2025-03-24 14:14:07 +0100

    fixed mobical without colro

--- src/terminal.hpp
+++ src/terminal.hpp
@@ -26,8 +26,8 @@ class Terminal {
   }
 
   void code (const char *str) {
-    if (!use_colors)
-      return;
+    // if (!use_colors)
+    // return;
     if (!connected)
       return;
     escape ();
