951774fbc34b1b9ba37b8d5b0beba32c9b7d6fab
Mate Soos <soos.mate@gmail.com>
2025-03-29 11:48:55 +0100

    Fixed emscripten build

--- build_emscripten.sh
+++ build_emscripten.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-export CXX=/usr/lib/emscripten/em++
-export CC=/usr/lib/emscripten/emcc
-export AR=/usr/lib/emscripten/emar
-export CXXFLAGS=-fPIC
-./configure_emscripten --no-unlocked --no-flexible
-make -j12
+emconfigure ./configure --no-contracts --no-tracing
+emmake make
