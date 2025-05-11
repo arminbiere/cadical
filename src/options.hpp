57ba7d0ea6d7f149fc9835ffc4ce4fcb37a02e45
Mathias Fleury <Mathias.Fleury@ens-rennes.fr>
2025-05-11 16:44:52 +0200

    set as default; bump version;

--- src/options.hpp
+++ src/options.hpp
@@ -113,7 +113,7 @@ OPTION( factor,            1,  0,  1,0,1,1, "bounded variable addition") \
 OPTION( factorcandrounds,  2,  0,2e9,0,0,1, "candidates reduction rounds") \
 OPTION( factoreffort,     50,  0,1e6,0,0,1, "relative effort per mille") \
 OPTION( factoriniticks,  300,  1,1e6,0,0,1, "initial effort in millions") \
-OPTION( factornoreconstr,  0,  0,  1,0,0,1, "push eliminated BVA on reconstruction") \
+OPTION( factornoreconstr,  1,  0,  1,0,0,1, "don't push unusued eliminated BVA on reconstruction") \
 OPTION( factorsize,        5,  2,2e9,0,0,1, "clause size limit") \
 OPTION( factorthresh,      7,  0,100,1,0,1, "delay if ticks smaller thresh*clauses") \
 OPTION( fastelim,          1,  0,  1,0,1,1, "fast BVE during preprocessing") \
