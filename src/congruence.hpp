9aae4f2aed72e191b8475e0db8c3c51e61f6d264
Florian Pollitt <pollittf@informatik.uni-freiburg.de>
2025-11-26 12:22:58 +0100

    default constructor

--- src/congruence.hpp
+++ src/congruence.hpp
@@ -274,7 +274,11 @@ struct Gate {
   vector<int> rhs;
 
   size_t arity () const { return rhs.size (); }
-  Gate () { garbage = indexed = marked = shrunken = 0; }
+  Gate () {
+    garbage = indexed = marked = shrunken = 0;
+    tag = Gate_Type::And_Gate;
+    lhs = 0;
+  }
 
   bool operator== (Gate const &lhs) {
     return tag == lhs.tag && rhs == lhs.rhs;
