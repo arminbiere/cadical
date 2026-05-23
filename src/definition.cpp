858c02016e1911c9033f0bd7a939cdd0c18f95fb
Mathias Fleury <fleury@cs.uni-freiburg.de>
2026-05-23 13:51:06 +0200

    prefix kitten to avoid clashing with kissat

--- src/definition.cpp
+++ src/definition.cpp
@@ -1,3 +1,5 @@
+#include "kitten-config.h"
+#include "kitten.h"
 #include "internal.hpp"
 
 namespace CaDiCaL {
@@ -203,7 +205,7 @@ void Internal::find_definition (Eliminator &eliminator, int lit) {
       // representation we instead implement the translation in kitten
       if (!c->garbage) {
         LOG (c, "adding to kitten");
-        citten_clause_with_id_and_exception (citten, exported, c->size,
+        KITTEN_NAMESPACE(citten_clause_with_id_and_exception) (citten, exported, c->size,
                                              c->literals, except);
       }
       exported++;
@@ -211,21 +213,21 @@ void Internal::find_definition (Eliminator &eliminator, int lit) {
   }
   stats.eliminate_def_check++;
   const size_t limit = opts.elimdefticks;
-  kitten_set_ticks_limit (citten, limit);
-  int status = kitten_solve (citten);
+  KITTEN_NAMESPACE(kitten_set_ticks_limit) (citten, limit);
+  int status = KITTEN_NAMESPACE(kitten_solve) (citten);
   if (!exported)
     goto ABORT;
   if (status == 20) {
     LOG ("sub-solver result UNSAT shows definition exists");
     uint64_t learned;
-    unsigned reduced = kitten_compute_clausal_core (citten, &learned);
+    unsigned reduced = KITTEN_NAMESPACE(kitten_compute_clausal_core) (citten, &learned);
     LOG ("1st sub-solver core of size %u original clauses out of %u",
          reduced, exported);
     for (int i = 2; i <= opts.elimdefcores; i++) {
-      kitten_shrink_to_clausal_core (citten);
-      kitten_shuffle_clauses (citten);
-      kitten_set_ticks_limit (citten, 10 * limit);
-      int tmp = kitten_solve (citten);
+      KITTEN_NAMESPACE(kitten_shrink_to_clausal_core) (citten);
+      KITTEN_NAMESPACE(kitten_shuffle_clauses) (citten);
+      KITTEN_NAMESPACE(kitten_set_ticks_limit) (citten, 10 * limit);
+      int tmp = KITTEN_NAMESPACE(kitten_solve) (citten);
       assert (!tmp || tmp == 20);
       if (!tmp) {
         LOG ("aborting core extraction");
@@ -234,7 +236,7 @@ void Internal::find_definition (Eliminator &eliminator, int lit) {
 #ifndef NDEBUG
       unsigned previous = reduced;
 #endif
-      reduced = kitten_compute_clausal_core (citten, &learned);
+      reduced = KITTEN_NAMESPACE(kitten_compute_clausal_core) (citten, &learned);
       LOG ("%d sub-solver core of size %u original clauses out of %u", i,
            reduced, exported);
       assert (reduced <= previous);
@@ -245,7 +247,7 @@ void Internal::find_definition (Eliminator &eliminator, int lit) {
     stats.eliminate_def_success++;
     eliminator.gatetype = DEF;
     eliminator.definition_unit = 0;
-    kitten_traverse_core_ids (citten, &extractor, traverse_definition_core);
+    KITTEN_NAMESPACE(kitten_traverse_core_ids) (citten, &extractor, traverse_definition_core);
     assert (eliminator.definition_unit);
     int unit = 0;
     if (eliminator.definition_unit == 2) {
@@ -261,11 +263,11 @@ void Internal::find_definition (Eliminator &eliminator, int lit) {
       if (proof) {
         if (lrat) {
           extractor.unit = unit;
-          kitten_trace_core (citten, &extractor,
-                             traverse_one_sided_core_lemma_with_lrat);
+          KITTEN_NAMESPACE(kitten_trace_core) (citten, &extractor,
+                               traverse_one_sided_core_lemma_with_lrat);
         } else {
           extractor.unit = unit;
-          kitten_traverse_core_clauses (citten, &extractor,
+          KITTEN_NAMESPACE(kitten_traverse_core_clauses) (citten, &extractor,
                                         traverse_one_sided_core_lemma);
         }
       } else
@@ -276,7 +278,7 @@ void Internal::find_definition (Eliminator &eliminator, int lit) {
   ABORT:
     LOG ("sub-solver failed to show that definition exists");
   }
-  stats.eliminate_def_ticks += kitten_current_ticks (citten);
+  stats.eliminate_def_ticks += KITTEN_NAMESPACE(kitten_current_ticks) (citten);
   return;
 }
 
