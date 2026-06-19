67c024cd04faeb159a99c14bec5b20c791f37a8d
Florian Pollitt <pollittf@informatik.uni-freiburg.de>
2026-06-19 14:34:40 +0200

    no tracing

--- src/external_propagate.cpp
+++ src/external_propagate.cpp
@@ -2,10 +2,11 @@
 
 namespace CaDiCaL {
 
-#ifndef NTRACING
 #define LOG_INTERACTION_START(NAME) LOG (#NAME " on level %d START", level);
 #define LOG_INTERACTION_FOR(NAME, VAL) \
   LOG (#NAME "(%d) on level %d START", VAL, level)
+
+#ifndef NTRACING
 
 static void trace_api_call (FILE *trace_api_file, Internal *internal,
                             const char *s0) {
@@ -61,17 +62,13 @@ static void trace_api_call (FILE *trace_api_file, Internal *internal,
     trace_api_call (external->trace_api_file, this, #NAME, VAL); \
   } while (0)
 #else
-#define LOG_INTERACTION_START(NAME) LOG (#NAME " on level %d START", level)
-#define LOG_INTERACTION_FOR(NAME, VAL) \
-  LOG (#NAME "(%d) on level %d START", VAL, level)
-
-#define LOG_INTERACTION_END(NAME) LOG (#NAME " on level %d END", level)
+#define LOG_INTERACTION_END(NAME) LOG (#NAME " on level %d END", level);
 #define LOG_INTERACTION_RETURN(NAME, VAL) \
-  LOG (#NAME "returns %d on level %d END", VAL, level)
-#define LOG_INTERACTION_END_FOR(NAME) \
-  LOG (#NAME "(%d) on level %d END", VAL, level)
+  LOG (#NAME " returns %d on level %d END", VAL, level);
 #define LOG_INTERACTION_RETURN_FOR(NAME, VAL, RET) \
-  LOG (#NAME "(%d) returns %d on level %d END", VAL, RET, level)
+  LOG (#NAME "(%d) returns %d on level %d END", VAL, RET, level);
+#define LOG_INTERACTION_END_FOR(NAME, VAL) \
+  LOG (#NAME "(%d) on level %d END", VAL, level);
 #endif
 
 /*----------------------------------------------------------------------------*/
