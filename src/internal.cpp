5e62e0eed01aeb8456e45d5f7fc51988c5fadcb1
Florian Pollitt <pollittf@informatik.uni-freiburg.de>
2026-09-07 08:33:31 +0200

    run lucky only once without preprocessing (related to #190)

--- src/internal.cpp
+++ src/internal.cpp
@@ -1016,16 +1016,19 @@ int Internal::solve (bool preprocess_only) {
     if (!res && !level)
       res = local_search ();
   }
-  bool run_lucky = stats.conflicts >=
-                   lim.lucky; // cannot be in lucky, because we run it twice
-  bool update_lucky_limits =
-      !opts.luckylate; // update in the second run if there is any
+  bool run_lucky = (stats.conflicts >= lim.lucky); // cannot be in lucky, because we run it twice
+  bool update_lucky_limits = !opts.luckylate; // update in the second
+  // run if there is any only run lucky late if some preprocessing was
+  // done, which corresponds to preprocesslight (only done during the
+  // first search), or if the user forced preprocessing, or there are
+  // full preprocessing round.
+  bool will_do_any_preprocessing = ((opts.preprocesslight && stats.searches <= 1) || preprocess_only || lim.preprocessing);
   if (!preprocess_only && !res && !level && opts.luckyearly && run_lucky)
     res = lucky_phases (update_lucky_limits);
   if (!res && !level)
     res = preprocess (preprocess_only);
   if (!preprocess_only) {
-    if (!res && !level && opts.luckylate && run_lucky)
+    if (!res && !level && will_do_any_preprocessing && opts.luckylate && run_lucky)
       res = lucky_phases (true);
     if (!res && !level)
       res = local_search ();
